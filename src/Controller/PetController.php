91fb049ff838c6d519ade6f75c799de2371ff250
Seth Stevenson <sethstevenson2005@gmail.com>
2025-12-06 15:27:10 -0400

    set up relationship between pet and breed entity in controller

--- src/Controller/PetController.php
+++ src/Controller/PetController.php
@@ -2,25 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Breed;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Pet;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\BreedRepository;
 
 class PetController extends AbstractController
 {
     #[Route('/pet', name: 'create_pet', methods: ['POST'])]
     public function createPet(Request $request, EntityManagerInterface $entityManager): Response
     {
+        /* Todo: Move these setters into the Entity Class */
         $pet = new Pet();
 
-        /* Todo: Move these setters into the model */
+        $breed = $entityManager->getRepository(Breed::class)->findBy([
+            'type' => $request->getPayload()->get('breed')
+        ]);
+
+        if (empty($breed)) {
+            $breed = new Breed();
+            $breed->setType($request->getPayload()->get('breed'));
+            $entityManager->persist($breed);
+        } else {
+            // Todo: this will be a loop, right now just return first
+            $breed = $breed[0];
+        }
+
         $pet->setType($request->getPayload()->get('type'));
         $pet->setName($request->getPayload()->get('name'));
         $pet->setSex($request->getPayload()->get('sex'));
+        $pet->addBreed($breed);
         $pet->setDateOfBirth($request->getPayload()->get('date_of_birth'));
 
         /* End Todo */
