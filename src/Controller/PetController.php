d54112fe1ce8ce19d3b1c557be0e78ab54016c35
Seth Stevenson <sethstevenson2005@gmail.com>
2025-12-06 14:21:59 -0400

    returning Json from the controller

--- src/Controller/PetController.php
+++ src/Controller/PetController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Pet;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,14 +13,21 @@ use Symfony\Component\Routing\Attribute\Route;
 class PetController extends AbstractController
 {
     #[Route('/pet', name: 'create_pet', methods: ['POST'])]
-    public function createPet(Request $request, EntityManagerInterface $entityManager): Response {
+    public function createPet(Request $request, EntityManagerInterface $entityManager): Response
+    {
         $pet = new Pet();
+
+        /* Todo: Move these setters into the model */
+        $pet->setType($request->getPayload()->get('type'));
         $pet->setName($request->getPayload()->get('name'));
         $pet->setSex($request->getPayload()->get('sex'));
+        $pet->setDateOfBirth($request->getPayload()->get('date_of_birth'));
+
+        /* End Todo */
 
         $entityManager->persist($pet);
         $entityManager->flush();
 
-        return new Response('Created Pet: ' . $pet->getName() );
+        return $this->json($pet);
     }
 }
