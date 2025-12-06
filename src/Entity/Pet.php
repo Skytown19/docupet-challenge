a5681fb66e351caa3edb860ebbd95d4585daebbe
Seth Stevenson <sethstevenson2005@gmail.com>
2025-12-06 14:21:37 -0400

    added and repairs some properties

--- src/Entity/Pet.php
+++ src/Entity/Pet.php
@@ -3,7 +3,12 @@
 namespace App\Entity;
 
 use App\Repository\PetRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Carbon\Carbon;
+
 
 #[ORM\Entity(repositoryClass: PetRepository::class)]
 class Pet
@@ -18,6 +23,23 @@ class Pet
 
     #[ORM\Column(length: 255)]
     private ?string $sex = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $dateOfBirth = null;
+
+    /**
+     * @var Collection<int, Breed>
+     */
+    #[ORM\ManyToMany(targetEntity: Breed::class)]
+    private Collection $breed;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    public function __construct()
+    {
+        $this->breed = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -36,7 +58,7 @@ class Pet
         return $this;
     }
 
-    public function getSex(): ?int
+    public function getSex(): ?string
     {
         return $this->sex;
     }
@@ -44,6 +66,55 @@ class Pet
     public function setSex(string $sex): static
     {
         $this->sex = $sex;
+
+        return $this;
+    }
+
+    public function getDateOfBirth(): ?\DateTime
+    {
+        return $this->dateOfBirth;
+    }
+
+    public function setDateOfBirth(?string $dateOfBirth): static
+    {
+        $dateOfBirth = Carbon::parse($dateOfBirth);
+        $this->dateOfBirth = $dateOfBirth;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Breed>
+     */
+    public function getBreed(): Collection
+    {
+        return $this->breed;
+    }
+
+    public function addBreed(Breed $breed): static
+    {
+        if (!$this->breed->contains($breed)) {
+            $this->breed->add($breed);
+        }
+
+        return $this;
+    }
+
+    public function removeBreed(Breed $breed): static
+    {
+        $this->breed->removeElement($breed);
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
 
         return $this;
     }
