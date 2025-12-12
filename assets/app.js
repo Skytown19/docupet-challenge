617b8940bedfce562ac5738fdac51539f1d0a9a7
Seth Stevenson <sethstevenson2005@gmail.com>
2025-12-12 09:59:04 -0400

    handling unknown age on the front end

--- assets/app.js
+++ assets/app.js
@@ -79,7 +79,9 @@ function domReady () {
     const datePicker = document.getElementById('petDatePicker');
     const mixedBreed = document.getElementById('mixedBreed');
     const mixedBreedInput = document.getElementById('petMixedBreed');
+    const approximateAgeContainer = document.getElementById('approximateAge');
     const approximateAgeInput = document.getElementById('petApproximateAge');
+    const dangerousBreedElement = document.getElementById('isDangerousAnimal');
 
     // Event Listeners
     petTypeDogButton.addEventListener('click', () => {
@@ -110,8 +112,8 @@ function domReady () {
         if (!petKnownDateOfBirthButton.classList.contains('selected')) {
             petUnknownDateOfBirthButton.classList.remove('selected');
             petKnownDateOfBirthButton.classList.add('selected');
-            if (!approximateAgeInput.hasAttribute('hidden')){
-                approximateAgeInput.toggleAttribute('hidden');
+            if (!approximateAgeContainer.hasAttribute('hidden')){
+                approximateAgeContainer.toggleAttribute('hidden');
             }
             datePicker.toggleAttribute('hidden');
         }
@@ -124,7 +126,7 @@ function domReady () {
             if (!datePicker.hasAttribute('hidden')){
                 datePicker.toggleAttribute('hidden');
             }
-            approximateAgeInput.toggleAttribute('hidden');
+            approximateAgeContainer.toggleAttribute('hidden');
         }
     });
 
@@ -135,12 +137,18 @@ function domReady () {
     backButton.addEventListener('click', () => {
         document.getElementById('dangerousAnimalForm').toggleAttribute('hidden');
         document.getElementById('results').toggleAttribute('hidden');
+
+        // Hide the dangerous breed element when resetting the form
+        if (!dangerousBreedElement.hasAttribute('hidden')) {
+            dangerousBreedElement.toggleAttribute('hidden');
+        }
     });
 
     document.getElementById('submit').addEventListener('click', async () => {
         // Set properties that aren't automatic
         pet.setName(document.getElementById('petName').value);
 
+        // Case for Mix & Unknonw Breed
         if (mixedCheckbox.checked) {
             console.log('HELLO');
             console.log(mixedBreedInput.value);
@@ -151,15 +159,35 @@ function domReady () {
             pet.setBreed(document.getElementById('petBreed').value);
         }
 
-        pet.setDateOfBirth(document.getElementById('default-datepicker').value);
+        // Case for Unknown Date of Birth
+        if (petUnknownDateOfBirthButton.classList.contains('selected')) {
+            let approximateAge = approximateAgeInput.value;
+            console.log(approximateAge);
+            let approximateBirthYear = new Date().getFullYear() - approximateAge;
+            console.log(approximateBirthYear);
+            let approximateBirthDate = new Date();
+            approximateBirthDate.setFullYear(approximateBirthYear);
+            console.log(approximateBirthDate);
+            pet.setDateOfBirth(approximateBirthDate.toISOString().slice(0, 10));
+        } else {
+            pet.setDateOfBirth(document.getElementById('default-datepicker').value);
+        }
+
         const response = await submitPet(pet);
         const result = await response.json();
 
+        let breed = result.breed.join(", ")
         document.getElementById('petNameResult').textContent = result.name;
         document.getElementById('petTypeResult').textContent = result.type;
-        document.getElementById('petBreedResult').textContent = result.breed.join(", ");
+        document.getElementById('petBreedResult').textContent = breed;
         document.getElementById('petSexResult').textContent = result.sex;
         document.getElementById('petDateOfBirthResult').textContent = result.dateOfBirth;
+        document.getElementById('petAgeResult').textContent = result.age;
+
+        if (result.dangerous && dangerousBreedElement.hasAttribute('hidden')) {
+            document.getElementById('breed').textContent = breed;
+            dangerousBreedElement.toggleAttribute('hidden');
+        }
 
         document.getElementById('dangerousAnimalForm').toggleAttribute('hidden');
         document.getElementById('results').toggleAttribute('hidden');
